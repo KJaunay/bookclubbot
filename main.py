797449a1716424c7763e0957f55e7306adee5694
unknown <kieran.jaunay@snapcomms.com>
2021-09-09 16:59:41 +1000

    add sendMessage function

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 #curl -X POST      -H "Authorization: Bearer $TOKEN"      -H 'Content-type: application/json;charset=utf-8'     --data '{"channel":"#general","text":"Hello, Slack!"}' https://slack.com/api/chat.postMessage
 
-from os import environ
+import os
+import logging
+from slack_sdk.web.client import WebClient
+from slack_sdk.errors import SlackApiError
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+def sendMessage(slack_client, msg):
+    try:   
+        logging.debug("inside call")
+        slack_client.chat_postMessage(
+            channel='#general',
+            text=msg
+        )
+
+    except SlackApiError as e:
+        logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
+        logging.error(e.response)
 
 if __name__ == "__main__":
-    #do stuff
-    print("hello, world!")
+    SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+    slack_client = WebClient(SLACK_BOT_TOKEN)
+    logging.debug("authorized slack client")
+    msg = "Good Morning!"
+    sendMessage(slack_client, msg)
