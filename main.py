1b3798eb9e963bf011c73c47416530d690d5d97a
unknown <kieran.jaunay@snapcomms.com>
2021-09-12 14:57:12 +1000

    change subcommand info to summary, add new info and chapter subcommands

--- main.py
+++ main.py
@@ -23,14 +23,42 @@ def process(ack, body, say, respond):
     subcommand=txt[0]
     if subcommand in valid_subcommands:
         book=get_book()
-        if subcommand == 'info':
-            msg="Title: {0}, Author: {1}".format(book['title'],book['author'])
+        if subcommand == 'summary':
+            msg="*Title:* {0},\n*Author*: {1},\n *Synopsis*: {2}".format(book['title'],book['author'],book['synopsis'])
             response={
                 "response_type": "in_channel",
                 "text": msg
             }
+            say(response) #responds back to channel 
+        elif subcommand == 'info':
+            msg="The company book club is an opportunity to read a book with your colleagues and then have a discussion to learn and gather insights into their perspective. Please contact {0} for more info.".format(book['coordinator'])
+            response={
+                "response_type": "in_channel",
+                "text": msg
+            }
+            say(response) #responds back to channel 
+        elif subcommand == 'chapter': 
+            chapter_number=int(txt[1].split(" ",1)[0])
+            if chapter_number <= len(book['chapters']):
+                chapter=book['chapters'][str(chapter_number)]
+                msg="*Chapter {0}*: {1},\n*Summary*: {2},\n*Questions*: {3}".format(chapter_number,chapter['title'],chapter['summary'],chapter['questions'])
+                response={
+                    "response_type": "in_channel",
+                    "text": msg
+                }
+                say(response) #responds back to channel 
+            else: 
+                msg="The chapter number you have specified ({0}), does not yet have any information available on it.".format(chapter_number)
+                respond(msg)
 
-        say(response) #responds back to channel 
+        else: 
+            msg="Thank you for choosing Bookclubbot, the subcommand \"{0}\", is still under construction!".format(subcommand)
+            response={
+                "response_type": "in_channel",
+                "text": msg
+            }
+            respond(msg)
+        
     else: 
         respond("The subcommand you have attempted to use is invalid. Please one of the following {0}".format(valid_subcommands))
 
